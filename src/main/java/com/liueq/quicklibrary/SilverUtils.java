4a57abb939cf8d45627df37feec076c8fe41c4b1
liueq <liueqmaaya@gmail.com>
2017-11-14 15:13:25 +0800

    Merge branch 'master' of https://github.com/liueq/QuickLibrary
    # Conflicts:
    #	build.gradle
    #	src/main/AndroidManifest.xml
    
    SilverUtils 增加了 MD5算法；
    从git 中去掉了 build.gradle。

--- src/main/java/com/liueq/quicklibrary/SilverUtils.java
+++ src/main/java/com/liueq/quicklibrary/SilverUtils.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.util.Log;
 import android.util.TypedValue;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by liueq on 9/10/2017.
  */
@@ -12,15 +15,33 @@ public class SilverUtils {
 
     private static final String TAG = "SilverUtils";
 
-    public static float dpToPixel(int dp, Context context){
+    public static float dpToPixel(int dp, Context context) {
         float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
         Log.d(TAG, "dpToFloat: " + px);
         return px;
     }
 
-    public static float spToPixel(int sp, Context context){
+    public static float spToPixel(int sp, Context context) {
         float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
         Log.d(TAG, "spToFloat: " + px);
         return px;
     }
+
+    public static String generateMd5(String str) {
+        String strPasswordMD5 = "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte buf[] = digest.digest(str.getBytes());
+            String stmp = null;
+            for (int n = 0; n < buf.length; n++) {
+                stmp = Integer.toHexString(buf[n] & 0xff);
+                strPasswordMD5 = stmp.length() == 1 ?
+                        (strPasswordMD5 + "0" + stmp) : (strPasswordMD5 + stmp);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return strPasswordMD5;
+    }
+
 }
